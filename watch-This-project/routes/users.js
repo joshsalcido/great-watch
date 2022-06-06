8c81c2b9105d241a4a5d26dc675a2d7861856550
Josh Salcido <joshuasalcido21@gmail.com>
2022-06-06 16:15:59 -0600

    initial user setup

--- watch-This-project/routes/users.js
+++ watch-This-project/routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/models');
+const bcrypt = require('bcryptjs');
+const { validationResult , check} = require('express-validator');
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
