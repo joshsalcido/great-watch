0c7c4de9664fd965c005728d92d43d12c80e0f29
Luis <sans.boba@gmail.com>
2022-06-06 12:37:12 -0700

    genre seeds

---
+++ watch-This-project/data/genre-seed-data.js
@@ -0,0 +1,49 @@
+const genres = [
+  /*==============Copy From Here...==============*/
+  {
+    genre: 'Action'
+  },
+  {
+    genre: 'Horror'
+  },
+  {
+    genre: 'Drama'
+  },
+  {
+    genre: 'Comedy'
+  },
+  {
+    genre: 'Thriller'
+  },
+  {
+    genre: 'Romance'
+  },
+  {
+    genre: 'Sci-Fi'
+  },
+  {
+    genre: 'Documentary'
+  },
+  {
+    genre: 'Fantasy'
+  },
+  {
+    genre: 'Adventure'
+  },
+  {
+    genre: 'Romantic Comedy'
+  },
+  {
+    genre: 'Epic'
+  },
+  {
+    genre: 'Biography'
+  },
+  {
+    genre: 'Crime'
+  },
+  {
+    genre: 'Sports'
+  }
+  /*=================... To Here=================*/
+]
