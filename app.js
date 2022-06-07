f141e60bdb9a08b1b0b7fc75aaf4b297e82e3c29
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-07 09:15:47 -0700

    finished the register page with route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { sequelize } = require("./db/models");
-const { restoreUser } = require('./auth');
+const { restoreUser } = require("./auth");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const indexRouter = require("./routes/index");
@@ -37,7 +37,7 @@ app.use(
 store.sync();
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
