2f9982cec8adef96495d566cc130256ef3ea3519
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-09 16:58:59 -0700

    added some css on / page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // Declare and Render Logged in user
-  res.render("index", { title: "WatchThis" });
+  res.render("index");
 });
 
 module.exports = router;
