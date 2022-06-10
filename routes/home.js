8172f1d90f80d7be61b4b9e6db0309b8ffb7ba5b
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2022-06-10 09:43:58 -0700

    displayed welcome message

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
-const { restoreUser, requireAuth } = require("../auth")
+const { restoreUser, requireAuth } = require("../auth");
+const db = require("../db/models");
 
-router.get("/home", restoreUser, requireAuth, (req, res) => {
-  res.render("home")
-})
+router.get("/home", restoreUser, requireAuth, async (req, res) => {
+  const userId = req.session.auth.userId;
+  const user = await db.User.findOne({
+    where: {
+      id: userId,
+    },
+  });
+  res.render("home", { user });
+});
 
-module.exports = router
+module.exports = router;
